04e0f87151fd6675143255bd6fed7ac7d9a57182
mechanicalcodingthunder <132193226+mechanicalcodingthunder@users.noreply.github.com>
2024-01-06 19:57:49 +0530

    Update content.js

--- static/js/content.js
+++ static/js/content.js
@@ -13,7 +13,19 @@ document.querySelector("#close").addEventListener("click", function () {
     document.querySelector(".popup_screen").style.zIndex = "-100";
     document.querySelector(".popup_screen").style.visibility = "hidden";
 });
+$(window).click(function () { 
+    hide_resource();
+    //Hide the menus if visible
+}); 
 function display_resource(){
     document.getElementById("dropdown").style.visibility="visible";
     document.getElementById("dropdown").style.opacity="1"
+    document.getElementById("dropdown").children[0].children[1].style.visibility="visible"
+    document.getElementById("dropdown").children[0].children[1].style.opacity="1";
 };
+function hide_resource(){
+    document.getElementById("dropdown").style.visibility="hidden";
+    document.getElementById("dropdown").style.opacity="0"
+    document.getElementById("dropdown").children[0].children[1].style.visibility="hidden"
+    document.getElementById("dropdown").children[0].children[1].style.opacity="0";
+}
